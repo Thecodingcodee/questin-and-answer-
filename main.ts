d687fa367e636cb9a8a41636b2ddabe0442f4c63
Thecodingcodee <166464327+Thecodingcodee@users.noreply.github.com>
2024-04-12 00:02:45 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,6 +3,7 @@ namespace SpriteKind {
     export const Complete = SpriteKind.create()
     export const Npc2 = SpriteKind.create()
     export const Npc3 = SpriteKind.create()
+    export const Npc4 = SpriteKind.create()
 }
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Npc1, function (sprite, otherSprite) {
     Dialog_mode = true
@@ -16,6 +17,13 @@ sprites.onOverlap(SpriteKind.Player, SpriteKind.Npc1, function (sprite, otherSpr
     } else {
     	
     }
+    Dialog_mode = false
+    pause(2000)
+})
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Npc4, function (sprite, otherSprite) {
+    Dialog_mode = true
+    game.showLongText("My village need your help defeat the winged darks go through a series of questions to save us", DialogLayout.Bottom)
+    story.showPlayerChoices("Ok", "Ok")
     Dialog_mode = false
     pause(2000)
 })
@@ -136,6 +144,25 @@ Doggo = sprites.create(img`
     . . . f f . . f f . . f f . . . 
     `, SpriteKind.Npc3)
 tiles.placeOnTile(Doggo, tiles.getTileLocation(15, 12))
+let Reckesha = sprites.create(img`
+    . . . . 2 2 2 2 2 e . . . . . . 
+    . . . 2 2 2 2 d 2 2 e . . . . . 
+    . . e 2 2 2 2 2 2 2 e . . . . . 
+    . . e 2 2 2 2 2 2 2 e . . . . . 
+    . . e 2 2 2 2 2 e f f c c . . . 
+    . . e e 2 2 e f f f f b c . . . 
+    . e e e f e 2 b f f f d c . . . 
+    e e 2 2 d f 2 1 1 1 1 b c . . . 
+    e e 2 2 d f e e c c c . . . . . 
+    b 1 1 d e 2 2 e e c . . . . . . 
+    . f f e 2 2 2 2 e . . . . . . . 
+    . . f f d d 2 2 f f d d . . . . 
+    . . f f d d e e f f d d . . . . 
+    . . . f f f f . . . . . . . . . 
+    . . e e e f f f . . . . . . . . 
+    . . e e e e f f f . . . . . . . 
+    `, SpriteKind.Npc4)
+tiles.placeOnTile(Reckesha, tiles.getTileLocation(9, 8))
 scene.cameraFollowSprite(Trent)
 Dialog_mode = false
 music.play(music.createSoundEffect(WaveShape.Sine, 5000, 0, 255, 0, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.LoopingInBackground)
@@ -152,6 +179,15 @@ forever(function () {
 forever(function () {
     if (Dialog_mode == false) {
         controller.moveSprite(Rosy)
+    } else if (Dialog_mode == true) {
+        controller.moveSprite(Trent, 0, 0)
+    } else {
+    	
+    }
+})
+forever(function () {
+    if (Dialog_mode == false) {
+        controller.moveSprite(Trent)
     } else if (Dialog_mode == true) {
         controller.moveSprite(Trent, 0, 0)
     } else {
